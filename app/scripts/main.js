c6bb06fff5224192bcfee4be3c0844b283bb825b
hsumyatthwe11 <hsumyatthwe2020@gmail.com>
2020-12-23 13:17:07 +0630

    First Commit

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global bb-app, $*/
+
+
+window.bb-app = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  init: function () {
+    'use strict';
+    console.log('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function () {
+  'use strict';
+  bb-app.init();
+});
